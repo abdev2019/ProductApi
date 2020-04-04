cef7573e42da837a34a93b5fc7155b86e7ddfa61
abdo abdev <redouani2002@gmail.com>
2020-04-04 09:05:12 +0100

    Add new method in ProductRepository

--- src/main/kotlin/com/product/dao/ProductRepository.kt
+++ src/main/kotlin/com/product/dao/ProductRepository.kt
@@ -1,10 +1,14 @@
 package com.product.dao
 
 import com.product.entities.Product
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 
 @Repository
 interface ProductRepository : JpaRepository<Product, Long> {
+    fun findAllByTitleLikeAndSubtitleLikeAndDescriptionLike(title: String, subtitle: String, description: String, pageable: Pageable)
+            : Page<Product>
 }
